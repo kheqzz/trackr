e3485e6beef061e773bb802e3a60320c130998af
keqing <hanifsaifuddin99@gmail.com>
2026-09-06 17:32:30 +0700

    add some feature and bugs

--- lib/page/input_tracker.dart
+++ lib/page/input_tracker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/flutter_spinbox.dart';
 import 'package:trackr/helper/helper.dart';
 
 class InputTracker extends StatefulWidget {
@@ -23,6 +24,7 @@ class InputTracker extends StatefulWidget {
 }
 
 class _InputTrackerState extends State<InputTracker> {
+  TextEditingController _valueController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,59 +64,200 @@ class _InputTrackerState extends State<InputTracker> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          children: [
-            Row(
-              spacing: 10,
-              children: [
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.red[50],
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey[300]!),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Latest'),
-                          Row(
-                            children: [
-                              widget.trackerUnit != ""
-                                  ? (widget.latestItem! != 0
-                                        ? Text(widget.latestItem.toString())
-                                        : Icon(Icons.remove))
-                                  : Text(widget.trackerUnit!),
-                            ],
-                          ),
-                        ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                spacing: 10,
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.red[50],
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.grey[400]!),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Latest'),
+                            Row(
+                              children: [
+                                widget.latestItem! != 0
+                                    ? Text(widget.latestItem.toString())
+                                    : Icon(
+                                        Icons.remove,
+                                        size: 40,
+                                        color: Colors.grey[600],
+                                      ),
+                                widget.trackerType == 'number'
+                                    ? Text(
+                                        widget.trackerUnit ?? "",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.grey[600],
+                                        ),
+                                      )
+                                    : Text('Items Logged'),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.red[50],
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey[300]!),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [Text('History')],
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.red[50],
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.grey[400]!),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Total Logged'),
+                            Row(
+                              children: [
+                                widget.totalLoggedItem! != 0
+                                    ? Text(widget.totalLoggedItem.toString())
+                                    : Icon(
+                                        Icons.remove,
+                                        size: 40,
+                                        color: Colors.grey[600],
+                                      ),
+                                widget.trackerType == 'number'
+                                    ? Text(
+                                        widget.trackerUnit ?? "",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.grey[600],
+                                        ),
+                                      )
+                                    : Text('Items Logged'),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showGeneralDialog(
+                        context: context,
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                          return Dialog(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    'Add Entry',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  TextField(
+                                    controller: _valueController,
+                                    keyboardType: widget.trackerType == 'number'
+                                        ? TextInputType.number
+                                        : TextInputType.text,
+                                    decoration: InputDecoration(
+                                      labelText: widget.trackerType == 'number'
+                                          ? 'Value (${widget.trackerUnit})'
+                                          : 'Enter text',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide(
+                                          color: Colors.grey[400]!,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 15),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Note (Optional)',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide(
+                                          color: Colors.grey[400]!,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('Cancel'),
+                                      ),
+                                      SizedBox(width: 10),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('Save'),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(
+                        context,
+                      ).colorScheme.secondaryContainer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      '+ Add Entry',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
